e4ea35622366cc5356a5aaaef91dfa4d0134afd3
JHM92 <jake.murdoch@live.com>
2025-09-23 21:51:23 +0100

    added queries folder and moved dropTables functionality to it's own file

---
+++ db/queries/drops.js
@@ -0,0 +1,9 @@
+const db = require("../connection.js");
+
+async function dropTables() {
+  await db.query(`DROP TABLE IF EXISTS properties;`);
+  await db.query(`DROP TABLE IF EXISTS property_types;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+}
+
+module.exports = dropTables;
